55aea448f9c5713a8ed5e43f20cf34a9af761c6d
Edwin de Jonge <edwindjonge@gmail.com>
2016-09-14 12:17:01 +0200

    tweak

--- R/dictionary_nl.R
+++ R/dictionary_nl.R
@@ -2,6 +2,11 @@
 #'
 #' Gets a Dutch hunspell dictionary
 #' @export
+#' @examples
+#' library(hunspell)
+#' hunspell(c("eitje","ijtje"), dict=dictionary_nl())
+#' hunspell_suggest("ijtje", dictionary_nl())
+#' hunspell_stem("eitje", dict=dictionary_nl())
 dictionary_nl <- function(){
   hunspell::dictionary( lang  = system.file("dict/nl.dic", package = "nlstem")
                       , cache = "TRUE"
--- R/set_dicpath_nl.R
+++ R/set_dicpath_nl.R
@@ -1,8 +1,12 @@
-#' Adds Dutch dictionary to Hunspell dicpath
+#' Adds Dutch dictionary to Hunspell search path
 #'
 #' Sets the environment variable \code{DICPATH} (if unset) to the package
 #' directory.
 #' @export
+#' @examples
+#' library(hunspell)
+#' set_dicpath_nl() # add dutch dictionary to hunspell search path
+#' hunspell(c("eitje", "ijtje"), dict="nl")
 set_dicpath_nl <- function(){
   DICPATH <- Sys.getenv("DICPATH")
   if (nchar(DICPATH) == 0){
